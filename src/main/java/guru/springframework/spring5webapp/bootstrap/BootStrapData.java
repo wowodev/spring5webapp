2a7f73f07596ccdf835f532256f2cc82d4139dfe
Liam <szwowak@gmail.com>
2021-02-08 21:59:47 +0100

    - pojos

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,53 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+	private final AuthorRepository authorRepository;
+
+	private final BookRepository bookRepository;
+
+	private final PublisherRepository publisherRepository;
+
+	public BootStrapData(
+			AuthorRepository authorRepository,
+			BookRepository bookRepository,
+			PublisherRepository publisherRepository) {
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Author eric = new Author("Eric", "Evans");
+		Book ddd = new Book("Domain Driven Design", "123123");
+		eric.getBooks().add(ddd);
+		ddd.getAuthors().add(eric);
+
+		authorRepository.save(eric);
+		bookRepository.save(ddd);
+
+		Author rod = new Author("Rod", "Johnson");
+		Book noEJB = new Book("J2EE Development without EJB", "981672378516");
+		rod.getBooks().add(noEJB);
+		noEJB.getAuthors().add(rod);
+
+		authorRepository.save(rod);
+		bookRepository.save(noEJB);
+
+		Publisher publisher = new Publisher("Szymi", "Centralna 15", "Warszawa", "Mazowieckie", "12-123");
+		publisherRepository.save(publisher);
+
+		System.out.println(publisherRepository.count());
+	}
+}
---
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,115 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+
+	private String street;
+
+	private String city;
+
+	private String state;
+
+	private String zipCode;
+
+	public Publisher() {
+	}
+
+	public Publisher(String name, String street, String city, String state, String zipCode) {
+		this.name = name;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Publisher{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", street='" + street + '\'' +
+				", city='" + city + '\'' +
+				", state='" + state + '\'' +
+				", zipCode='" + zipCode + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Publisher publisher = (Publisher) o;
+
+		return Objects.equals(id, publisher.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+}
